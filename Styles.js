6c5b49c071a3dac385c058d01b81088a061871c2
ahmadhuzaifi98 <44748431+ahmadhuzaifi98@users.noreply.github.com>
2020-08-15 14:04:55 +0800

    Add files via upload

---
+++ Styles.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+
+
+
+export const Styles = StyleSheet.create({
+
+    
+
+    welcome: {
+
+      fontSize: 20,
+
+      textAlign: 'center',
+
+      margin: 0,
+
+    },
+
+    instructions: {
+
+      textAlign: 'center',
+
+      color: '#333333',
+
+      marginBottom: 5,
+
+    },
+
+  });
